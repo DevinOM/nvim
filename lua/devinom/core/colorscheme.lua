b3983b5a90d7cbb0c3d262cab70fee52fb5f8808
Devin Madrigal <devinmadr210@gmail.com>
2023-05-02 16:56:00 -0500

    Make transparency peristent

--- lua/devinom/core/colorscheme.lua
+++ lua/devinom/core/colorscheme.lua
@@ -1,17 +1,23 @@
-local status, _ = pcall(vim.cmd, "colorscheme rose-pine-moon")
+local status, _ = pcall(vim.cmd, "colorscheme rose-pine")
 if not status then
 	print("Colorscheme not found!")
 	return
 end
 
+require("rose-pine").setup({
+	disable_background = true,
+})
+
 function CMP(color)
-	color = color or "rose-pine-moon"
+	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 	-- make gitsigns plugin bar transparent
+	vim.cmd("highlight SignColumn guibg=none")
 	vim.cmd([[highlight GitSignsAdd guibg=NONE]])
 	vim.cmd([[highlight GitSignsChange guibg=NONE]])
 	vim.cmd([[highlight GitSignsDelete guibg=NONE]])
+	vim.cmd("hi NvimTreeNormal guibg=NONE")
 end
 CMP()
